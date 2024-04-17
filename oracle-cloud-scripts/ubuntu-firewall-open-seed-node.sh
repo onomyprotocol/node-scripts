9058e8b02ad497bedd982279ed70e56857419a95
AlexOnomy <alex@onomy.io>
2024-04-17 05:30:41 -0700

    oracle-cloud-scripts/ubuntu-firewall-open-seed-node.sh

---
+++ oracle-cloud-scripts/ubuntu-firewall-open-seed-node.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Oracle Cloud Ubuntu Firewall Config
+IPTABLES_CONFIG=/etc/iptables/rules.v4
+if test -f "$IPTABLES_CONFIG"; then
+  # Oracle Cloud Ubuntu Firewall Config
+  sudo sed -i 's/22 -j ACCEPT/&\n-A INPUT -p tcp -m state --state NEW -m tcp --dport 26656 -j ACCEPT/' $IPTABLES_CONFIG
+  sudo sed -i 's/22 -j ACCEPT/&\n-A INPUT -p tcp -m state --state NEW -m tcp --dport 26657 -j ACCEPT/' $IPTABLES_CONFIG
+  sudo iptables-restore < $IPTABLES_CONFIG
+fi
